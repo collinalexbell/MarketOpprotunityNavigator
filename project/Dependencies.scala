adaea2f2b61ce2d5564bcc1e874bebcc555b626c
kuberlog <collinalexbell@gmail.com>
2020-08-03 08:57:14 -0500

    added slick to deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,4 +2,6 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.1"
+  lazy val slick = "com.typesafe.slick" %% "slick" % "3.0.0"
+  lazy val slf4j = "org.slf4j" % "slf4j-nop" % "1.6.4"
 }
